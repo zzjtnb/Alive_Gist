e7f0030f3c3dd0fe46518a94cd9a87cf81425238
争逐 <zhengzhujituan@gmail.com>
2020-04-10 12:59:41 +0800

    first commit

---
+++ src/store/getters.js
@@ -0,0 +1,26 @@
+const getters = {
+    /**
+     * search
+     */
+    SearchValue: state => state.common.searchValue,
+    /**
+     * Gist
+     */
+    Query: state => state.gist.query,
+    /**
+     * 显示隐藏
+     */
+    networkSuccess: state => state.common.networkSuccess,
+    Mobile: state => state.common.Mobile,
+    showSide: state => state.common.ShowSideBar,
+    token: state => state.token.token,
+    /**
+     * Record
+     */
+    xianhua_num: state => state.common.xianhua_num,
+    dianzhu_num: state => state.common.dianzhu_num,
+    jingjiu_num: state => state.common.jingjiu_num,
+    jingli_num: state => state.common.jingli_num,
+    jugong_num: state => state.common.jugong_num,
+}
+export default getters
